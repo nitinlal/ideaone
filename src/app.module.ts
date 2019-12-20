7dc3208385cb701278db7e76098df613e562bd0d
nitinlal <nklal4@gmail.com>
2019-12-20 04:04:37 -0800

    feat: add dyamic module for config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { StatusController } from './status/status.controller';
 import { LocationController } from './location/location.controller';
 import { LocationService } from './location/location.service';
+import { LocationModule } from './location/location.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, StatusController, LocationController],
-  providers: [AppService, LocationService],
+  imports: [LocationModule, ConfigModule.register()],
+  controllers: [AppController, StatusController],
+  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/config/config.module.ts
@@ -0,0 +1,19 @@
+import { Module, DynamicModule } from '@nestjs/common';
+import { ConfigService } from './config.service';
+
+@Module({})
+export class ConfigModule {
+  static register(options: { folder: string }): DynamicModule {
+    return {
+      module: ConfigModule,
+      providers: [{ provide: 'CONFIG_OPTIONS', useValue: options }, ConfigService],
+      exports: [ConfigService]
+    }
+  }
+}
+
+
+// module contains the service and the contoller 
+// app module  ---> config module ---> configservice 
+// dynamic modules 
+// app module --> configmodule with params --> configService with params
